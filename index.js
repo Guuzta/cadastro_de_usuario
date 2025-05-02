1f8e23706f7bcd1d3e85a5035fc2a05a7950e519
Guuzta <Guuhsilva13@outlook.com>
2025-05-02 18:40:14 -0300

    adicionando estilos na pagina

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
 
 
 app.get('/', (req, res) => {
-    res.render('formulario')
+    res.render('home')
+})
+
+app.get('/cadastro', (req, res) => {
+    res.render('cadastro')
+})
+
+app.get('/usuarios', (req,res) => {
+    res.render('usuarios')
 })
 
 app.use((req, res) => {
